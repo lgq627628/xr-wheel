4112857fd4eb28b86c8862833cb2ca7e4bbec832
Blake <lgq627628@163.com>
2019-07-09 23:12:54 +0800

    feat: 使用chai-spies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Vue from '../node_modules/vue/dist/vue.common.js';
 import chai from 'chai';
+import spies from 'chai-spies'
 import Button from './button';
 import ButtonGroup from './button-group';
 import Icon from './icon';
@@ -7,6 +8,8 @@ import Icon from './icon';
 Vue.component('xr-button', Button);
 Vue.component('xr-button-group', ButtonGroup);
 Vue.component('xr-icon', Icon);
+
+chai.use(spies);
 
 new Vue({
     el: '#app',
@@ -21,56 +24,56 @@ const expect = chai.expect;
     // 测试图标
     // 先有实例，因为 button 是个对象，不能实例化，所以得 extend
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'setting'
         }
     });
-    // button.$mount('#test');
-    button.$mount();
-    let useEle = button.$el.querySelector('use');
+    // vm.$mount('#test');
+    vm.$mount();
+    let useEle = vm.$el.querySelector('use');
     let href = useEle.getAttribute('xlink:href');
     expect(href).to.eq('#icon-setting');
-    button.$el.remove(); // 删元素
-    button.$destroy(); // 删对象
+    vm.$el.remove(); // 删元素
+    vm.$destroy(); // 删对象
 }
 {
     // 测试 loading
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'setting',
             loading: true
         }
     });
-    button.$mount();
-    let useEle = button.$el.querySelector('use');
+    vm.$mount();
+    let useEle = vm.$el.querySelector('use');
     let href = useEle.getAttribute('xlink:href');
     expect(href).to.eq('#icon-loading');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     // 测试图标顺序
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'setting'
         }
     });
     const div = document.createElement('div');
     document.body.appendChild(div);
-    button.$mount(div); // 得渲染元素才行
-    let svg  = button.$el.querySelector('svg');
+    vm.$mount(div); // 得渲染元素才行
+    let svg  = vm.$el.querySelector('svg');
     let { order } = window.getComputedStyle(svg);
     expect(order).to.eq('1');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     // 测试图标顺序
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'setting',
             iconPos: 'right'
@@ -78,26 +81,29 @@ const expect = chai.expect;
     });
     const div = document.createElement('div');
     document.body.appendChild(div);
-    button.$mount(div); // 得渲染元素才行
-    let svg  = button.$el.querySelector('svg');
+    vm.$mount(div); // 得渲染元素才行
+    let svg  = vm.$el.querySelector('svg');
     let { order } = window.getComputedStyle(svg);
     expect(order).to.eq('2');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     // 测试点击事件
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'setting'
         }
     });
-    button.$mount();
-    button.$on('click', function() {
-        // expect(1).to.eq(2);
-    }) 
-    button.$el.click();
-    button.$el.remove();
-    button.$destroy();
+    vm.$mount();
+    let spy = chai.spy(() => {})
+    vm.$on('click', spy);
+    // vm.$on('click', function() {
+    //     expect(1).to.eq(2); 可以这样写但不对
+    // });
+    vm.$el.click();
+    expect(spy).to.have.been.called(); // 间谍函数被调用
+    vm.$el.remove();
+    vm.$destroy();
 }
