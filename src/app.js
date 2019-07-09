6549b648433ebb1071fb2c15e3f63857bb014818
Blake <lgq627628@163.com>
2019-07-09 21:33:28 +0800

    feat: 添加button组组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from '../node_modules/vue/dist/vue.common.js';
 import Button from './button';
+import ButtonGroup from './button-group';
 import Icon from './icon';
 
 Vue.component('xr-button', Button);
+Vue.component('xr-button-group', ButtonGroup);
 Vue.component('xr-icon', Icon);
 
 new Vue({
