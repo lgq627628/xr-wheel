8d6c13fe2e4333585359c0abefd89b15afaaa85a
Blake <lgq627628@163.com>
2019-07-11 23:03:37 +0800

    feat: 引入自动化测试

---
+++ test/button.test.js
@@ -0,0 +1,85 @@
+import Vue from 'vue'
+import chai from 'chai'
+import sinon from 'sinon'
+import Button from '../src/button'
+const expect = chai.expect;
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Button', () => { // BDD 行为驱动测试，属于mocha
+
+  it('存在.', () => { // 作用域隔绝
+    expect(Button).to.exist // 不是 undefined、null、0、''等 fasly 值，to，be是虚词，帮助语义化而已
+  })
+
+  it('可以设置icon.', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting'
+      }
+    }).$mount()
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
+    vm.$destroy()
+  })
+
+  it('可以设置loading.', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+        loading: true
+      }
+    }).$mount()
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
+    vm.$destroy()
+  })
+
+  it('icon 默认的 order 是 1', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+
+  it('设置 iconPosition 可以改变 order', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+        iconPos: 'right'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('点击 button 触发 click 事件', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+      }
+    }).$mount()
+
+    const callback = sinon.fake(); // 如何知道函数被调用
+    vm.$on('click', callback)
+    vm.$el.click()
+    expect(callback).to.have.been.called
+  })
+})
