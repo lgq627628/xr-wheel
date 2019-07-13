f58bb03c4f88a43c6f8205e5f9e0bdee8f3c84c5
Blake <lgq627628@163.com>
2019-07-13 20:45:55 +0800

    feat: 栅格布局雏形

---
+++ src/row.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="xr-row">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrRow',
+    props: {
+        gutter: {
+            type: Number,
+            default: 20
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.xr-row {
+    display: flex;
+}
+</style>
