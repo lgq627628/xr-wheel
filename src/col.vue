06cce9fb3f455e8693bc49f483c8f65baba76dca
Blake <lgq627628@163.com>
2019-07-13 21:28:54 +0800

    feat: 添加栅格参数 gutter 和 span

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="xr-col" :class="span ? `xr-col-${span}` : ''">
+    <div
+        class="xr-col"
+        :class="[
+            offset ? `xr-col-offset-${offset}` : '',
+            span ? `xr-col-${span}` : ''
+        ]"
+        :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
         <slot></slot>
     </div>
 </template>
@@ -7,12 +13,18 @@
 export default {
     name: 'XrCol',
     props: {
-        span: [Number, String]
+        span: [Number, String],
+        offset: [Number, String]
+    },
+    data() {
+        return {
+            gutter: 0
+        }
     }
 }
 </script>
 <style lang="scss" scoped>
-.xr-col {
+.xr-col { // 这个可以做迷宫
     height: 100px;
     width: 50%; // 给 50% 是因为 nowrap 和 shrink，多了会自动分配
     background: yellow;
@@ -20,6 +32,11 @@ export default {
     @for $n from 1 through 24 {
         &.xr-col-#{$n} {
             width: ($n / 24) * 100%;
+        }
+    }
+    @for $n from 1 through 24 {
+        &.xr-col-offset-#{$n} {
+            margin-left: ($n / 24) * 100%;
         }
     }
 }
