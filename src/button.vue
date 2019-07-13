a9702496fdc3fb53a5d1c6bd9301b58680fd4387
Blake <lgq627628@163.com>
2019-07-13 17:53:36 +0800

    feat: 新增 input 组件并写好单元测试

--- src/button.vue
+++ src/button.vue
@@ -34,7 +34,7 @@ export default {
     }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .xr-button {
     display: inline-flex;
     align-items: center;
--- src/icon.vue
+++ src/icon.vue
@@ -15,7 +15,7 @@ export default {
     }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .xr-icon {
     width: 1em;
     height: 1em;
