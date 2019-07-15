eaadc6ac627a8a0fd4730517ad284b6abcc5d1d6
Blake <lgq627628@163.com>
2019-07-15 22:12:45 +0800

    refactor: 小重构 col 组件

--- src/col.vue
+++ src/col.vue
@@ -49,31 +49,33 @@ export default {
     },
     computed: {
         colClass() {
-            let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
             // 把 js 的变化体现在 html 上，从而体现在 css 上
-            let phoneClass = phone && [
-                phone.span ? `xr-col-${phone.span}--phone` : '',
-                phone.offset ? `xr-col-offset-${phone.offset}--phone` : ''
-            ] || []
-            let ipadClass = ipad && [
-                ipad.span ? `xr-col-${ipad.span}--ipad` : '',
-                ipad.offset ? `xr-col-offset-${ipad.offset}--ipad` : ''
-            ] || []
-            let narrowPcClass = narrowPc && [
-                narrowPc.span ? `xr-col-${narrowPc.span}--narrowPc` : '',
-                narrowPc.offset ? `xr-col-offset-${narrowPc.offset}--narrowPc` : ''
-            ] || []
-            let pcClass = pc && [
-                pc.span ? `xr-col-${pc.span}--pc` : '',
-                pc.offset ? `xr-col-offset-${pc.offset}--pc` : ''
-            ] || []
+            // 方法一：
+            // let {span, offset} = this
+            // let screenArr = [];
+            // ['phone', 'ipad', 'narrowPc', 'pc', 'widePc'].forEach(screen => {
+            //     let nowScreen = this[screen];
+            //     let screenClass = nowScreen && [
+            //         nowScreen.span ? `xr-col-span-${nowScreen.span}--${screen}` : '',
+            //         nowScreen.offset ? `xr-col-offset-${nowScreen.offset}--${screen}` : ''
+            //     ] || []
+            //     screenArr.push(screenClass);
+            // })
+            // return [
+            //     offset ? `xr-col-offset-${offset}` : '',
+            //     span ? `xr-col-span-${span}` : '',
+            //     ...screenArr
+            // ]
+
+            // 方法二：
+            let {span, offset, phone, ipad, narrowPc, pc, widePc, createClass} = this
             return [
-                offset ? `xr-col-offset-${offset}` : '',
-                span ? `xr-col-${span}` : '',
-                ...phoneClass,
-                ...ipadClass,
-                ...narrowPcClass,
-                ...pcClass
+                ...createClass({span, offset}),
+                ...createClass(phone, '--phone'),
+                ...createClass(ipad, '--ipad'),
+                ...createClass(narrowPc, '--narrowPc'),
+                ...createClass(pc, '--pc'),
+                ...createClass(widePc, '--widePc'),
             ]
         },
         colStyle() {
@@ -81,6 +83,15 @@ export default {
                 paddingLeft: this.gutter/2 + 'px',
                 paddingRight: this.gutter/2 + 'px'
             }
+        }
+    },
+    methods: {
+        createClass (obj, screenName = '') {
+            let arr = [];
+            if (!obj) return arr;
+            if (obj.span) arr.push(`xr-col-span-${obj.span}${screenName}`);
+            if (obj.offset) arr.push(`xr-col-offset-${obj.offset}${screenName}`);
+            return arr;
         }
     }
 }
@@ -93,7 +104,7 @@ export default {
     //     flex: 1;
     // }
     @for $n from 1 through 24 {
-        &.xr-col-#{$n} {
+        &.xr-col-span-#{$n} {
             width: ($n / 24) * 100%;
         }
     }
@@ -104,7 +115,7 @@ export default {
     }
     @media (min-width: 0) { // 把手机样式写在这里，当屏幕缩小时，会覆盖上面的样式
         @for $n from 1 through 24 {
-        &.xr-col-#{$n}--phone {
+        &.xr-col-span-#{$n}--phone {
             width: ($n / 24) * 100%;
         }
         }
@@ -116,7 +127,7 @@ export default {
     }
     @media (min-width: 577px) {
         @for $n from 1 through 24 {
-        &.xr-col-#{$n}--ipad {
+        &.xr-col-span-#{$n}--ipad {
             width: ($n / 24) * 100%;
         }
         }
@@ -128,7 +139,7 @@ export default {
     }
     @media (min-width: 769px) {
         @for $n from 1 through 24 {
-        &.xr-col-#{$n}--narrowPc {
+        &.xr-col-span-#{$n}--narrowPc {
             width: ($n / 24) * 100%;
         }
         }
@@ -140,12 +151,24 @@ export default {
     }
     @media (min-width: 993px) {
         @for $n from 1 through 24 {
-        &.xr-col-#{$n}--pc {
+        &.xr-col-span-#{$n}--pc {
             width: ($n / 24) * 100%;
         }
         }
         @for $n from 1 through 24 {
             &.xr-col-offset-#{$n}--pc {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+    @media (min-width: 1200px) {
+        @for $n from 1 through 24 {
+        &.xr-col-span-#{$n}--pc {
+            width: ($n / 24) * 100%;
+        }
+        }
+        @for $n from 1 through 24 {
+            &.xr-col-offset-#{$n}--widePc {
                 margin-left: ($n / 24) * 100%;
             }
         }
