8b415977d2823eb5e4fd924ad88348ee29392c59
Blake <lgq627628@163.com>
2019-07-17 00:14:11 +0800

    feat: 扩充 toast 组件参数

--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,11 @@
 import Toast from './toast'
 export default {
     install(Vue, options) {
-        Vue.prototype.$toast = function(msg) {
+        Vue.prototype.$toast = function(msg, toastOpts) { // 我们调用时传递的参数
             let Constrouctor = Vue.extend(Toast)
-            let toast = new Constrouctor()
+            let toast = new Constrouctor({
+                propsData: toastOpts
+            })
             toast.$slots.default = [msg]
             toast.$mount() // 这个要放在 slot 后面
             document.body.appendChild(toast.$el)
--- src/toast.vue
+++ src/toast.vue
@@ -1,28 +1,96 @@
 <template>
-    <div class="xr-toast">
-        <slot></slot>
+    <div class="xr-toast" ref="toast">
+        <div v-if="enableHtml" v-html="$slots.default[0]"></div>
+        <slot v-else></slot>
+        <div class="xr-toast__line" ref="line"></div>
+        <span class="xr-toast__close" v-if="closeBtn" @click="handleClose">{{closeBtn.text}}</span>
     </div>
 </template>
 <script>
 export default {
-    name: 'XrToast'
+    name: 'XrToast',
+    props: {
+        autoClose: {
+            type: Boolean,
+            default: true
+        },
+        duration: {
+            type: Number,
+            default: 300
+        },
+        closeBtn: {
+            type: Object,
+            default: () => {
+                return {
+                    text: '关闭',
+                    cb: null
+                }
+            }
+        },
+        enableHtml: {
+            type: Boolean,
+            default: false
+        }
+    },
+    mounted() {
+        this.execAutoClose()
+        this.updateStyle();
+    },
+    methods: {
+        execAutoClose() {
+            if (this.autoClose) {
+                setTimeout(() => {
+                    this.close();
+                }, this.duration * 1000);
+            }
+        },
+        updateStyle() {
+            this.$nextTick(() => {
+                this.$refs.line.style.height = this.$refs.toast.getBoundingClientRect().height + 'px'
+            })
+        },
+        close() {
+            this.$el.remove()
+            this.$destroy()
+        },
+        handleClose() {
+            if (this.closeBtn && typeof this.closeBtn.cb === 'function') {
+                this.closeBtn.cb(this); // 可以把 this 回传给外面调用的地方
+            }
+            this.close()
+        },
+        log() {
+            console.log('我居然能被外部调用')
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
 $font-size: 14px;
-$height: 40px;
+$min-height: 40px;
 $bg-color: rgba(0, 0, 0, 0.7);
 .xr-toast {
+    display: flex;
+    align-items: center;
     position: fixed;
     top: 15vh;
     left: 50%;
     padding: 0 16px;
-    height: $height;
-    line-height: $height;
+    min-height: $min-height;
     transform: translateX(-50%);
     font-size: $font-size;
     background: $bg-color;
     border-radius: 4px;
     color: #fff;
+    &__line {
+        margin-left: 16px;
+        height: 100%;
+        border-left: 1px solid #ddd;
+    }
+    &__close {
+        flex-shrink: 0;
+        padding-left: 16px;
+        cursor: pointer;
+    }
 }
 </style>
