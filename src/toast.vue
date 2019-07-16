41d5c3d8e396940505aa1d5fbb6a14e43acc9131
Blake <lgq627628@163.com>
2019-07-16 21:32:49 +0800

    feat: 添加 toast 组件雏形

---
+++ src/toast.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="xr-toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrToast'
+}
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$height: 40px;
+$bg-color: rgba(0, 0, 0, 0.7);
+.xr-toast {
+    position: fixed;
+    top: 15vh;
+    left: 50%;
+    padding: 0 16px;
+    height: $height;
+    line-height: $height;
+    transform: translateX(-50%);
+    font-size: $font-size;
+    background: $bg-color;
+    border-radius: 4px;
+    color: #fff;
+}
+</style>
---
+++ src/tpl.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="xr-tpl">
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrTpl'
+}
+</script>
+<style lang="scss" scoped>
+.xr-tpl {
+   
+}
+</style>
