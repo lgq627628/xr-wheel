2904d817d4cd5a6303c9bb2814076d67d54fa0ef
Blake <lgq627628@163.com>
2019-07-21 17:39:27 +0800

    feat: 布局组件

---
+++ src/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="xr-header">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrHeader'
+}
+</script>
+<style lang="scss" scoped>
+.xr-header {
+   
+}
+</style>
---
+++ src/layout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="xr-layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrLayout',
+    data() {
+        return {
+            layoutClass: ''
+        }
+    },
+    mounted() {
+        let hasSider = this.$children.findIndex(vm => {
+            return vm.$options.name === 'XrSider'
+        }) >= 0
+        if (hasSider) this.layoutClass = 'has-sider'
+    }
+}
+</script>
+<style lang="scss" scoped>
+.xr-layout {
+   display: flex;
+   flex-direction: column;
+   flex-grow: 1;
+   &.has-sider {
+       flex-direction: row;
+   }
+}
+</style>
