78cf107b31035f2e8b3ded8f7781c42977536aba
Blake <lgq627628@163.com>
2019-07-29 21:50:00 +0800

    feat: 编写 collapse 组件雏形

---
+++ src/collapse.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="xr-collapse">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrCollapse'
+}
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+.xr-collapse {
+   border: 1px solid $grey;
+}
+</style>
