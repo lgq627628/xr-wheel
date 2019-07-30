fd865032d90c3e45c4decdb64c08407f3755a6fb
Blake <lgq627628@163.com>
2019-07-30 21:21:47 +0800

    feat: 接入 vuepress 写文档

---
+++ docs/.vuepress/components/button-demo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+		<xr-button>点我呀</xr-button>
+		<xr-button icon="setting" icon-pos="right">点我呀</xr-button>
+		<xr-button icon="loading">点我呀</xr-button>
+        <pre>
+            <code>{{content}}</code>
+        </pre>
+    </div>
+</template>
+
+<script>
+import XrButton from '../../../src/button';
+export default {
+    components: {
+        XrButton
+    },
+    data() {
+        return {
+            content: `
+                <xr-button>点我呀</xr-button>
+                <xr-button icon="setting" icon-pos="right">点我呀</xr-button>
+                <xr-button icon="loading">点我呀</xr-button>
+            `
+        }
+    }
+}
+</script>
---
+++ docs/components/button.md
@@ -0,0 +1,5 @@
+# 按钮
+
+使用方法
+
+<button-demo></button-demo>
