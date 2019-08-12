2df51e34f63d9c9b7daa1f5b55726eaf2b28a356
Blake <lgq627628@163.com>
2019-08-12 22:31:32 +0800

    feat: 优化 click-outside

--- src/click-outside.js
+++ src/click-outside.js
@@ -1,9 +1,13 @@
+document.addEventListener('click', e => { // 这样子写的话全局就一个事件监听，也没有必要移除掉
+    let target = e.target
+    clickEventList.forEach(item => {
+        if(item.el === target || item.el.contains(target)) return
+        item.cb()
+    });
+})
+let clickEventList = []
 export default {
     bind: function (el, binding, vnode) {
-        document.addEventListener('click', e => {
-            let target = e.target
-            if (el === target || el.contains(target)) return
-            binding.value()
-        })
+        clickEventList.push({el, cb: binding.value})
     }
 }
