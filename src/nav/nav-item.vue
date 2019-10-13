6d2f30cd4d225f321c7b1da3033a834ead646c24
Blake <lgq627628@163.com>
2019-10-13 14:54:47 +0800

    feat: nav组件雏形

---
+++ src/nav/nav-item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="xr-nav-item">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrNavItem'
+}
+</script>
+<style lang="scss" scoped>
+.xr-nav-item {
+   
+}
+</style>
---
+++ src/nav/nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="xr-nav">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'XrNav'
+}
+</script>
+<style lang="scss" scoped>
+.xr-nav {
+   
+}
+</style>
