41d6f232528a97ce04e5b685344d461aa6ddf071
lgq627628@163.com <lgq627628@163.com>
2021-06-20 11:19:29 +0800

    feat: 增加 tags-input 组件

---
+++ docs/.vuepress/components/tags-input-demo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+		<tags-input v-model="tags"></tags-input>
+        <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+
+<script>
+import TagsInput from '../../../src/tags-input/tags-input';
+export default {
+    components: {
+        TagsInput
+    },
+    data() {
+        return {
+            tags: ['111', '222', '333', '尤水就下'],
+            content: `
+                <tags-input v-model="tags"></tags-input>
+            `.replace(/\t+| +/g, '').trim()
+        }
+    }
+}
+</script>
